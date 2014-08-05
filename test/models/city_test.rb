63847694e01483b231d810163ef9ee18cb95a0e3
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-05 22:56:23 +0200

    Make test for associations

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -14,4 +14,10 @@ class CityTest < ActiveSupport::TestCase
       assert dub.errors.messages[:city_name], "No errors"
     end
 
+    test "city should have contacts associations" do
+      city_contacts = cities(:opole).contacts
+      assert_equal true, city_contacts, "City dont have associations"
+    end
+
+
 end
