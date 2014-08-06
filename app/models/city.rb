2a126f918a6320acbdd95be93ac9a9adef9f3d43
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-06 20:01:21 +0200

    Add self function for model City to sort Cities by count of contacts desc

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,4 +4,11 @@ class City < ActiveRecord::Base
 
   validates :city_name, uniqueness: true
 
+
+  def self.sort_all
+    s = Contact.group(:city_id).order('object DESC').select(:city_id)
+    s = s.map {|el| el.city_id}
+    s.map {|index| City.find(index)}
+  end
+
 end
