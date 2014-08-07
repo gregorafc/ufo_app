7d4fca4be957a20987975d12ff8d86065239635c
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-07 21:23:17 +0200

    Index sort repair

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,7 +6,7 @@ class City < ActiveRecord::Base
 
 
   def self.sort_all
-    s = Contact.group(:city_id).order('object_number DESC').select(:city_id)
+    s = Contact.group(:city_id).order('object_number').select(:city_id)
     s = s.map {|el| el.city_id}
     s.map {|index| City.find(index)}
   end
