44463554699dcadb06edce393aac5f52fe9bcb99
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-10 20:56:49 +0200

    Resource User add for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users
+
   get 'contacts/new'
 
   get 'contacts/create'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805191300) do
+ActiveRecord::Schema.define(version: 20140810185103) do
 
   create_table "cities", force: true do |t|
     t.string   "city_name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20140805191300) do
     t.date     "date_contact"
     t.integer  "object_number"
     t.integer  "city_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "login"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
